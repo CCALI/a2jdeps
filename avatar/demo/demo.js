24586978e57118482372eebaf63bccdbd592beb5
Mike 'mitch' Mitchel <mike@mikemitchel.com>
2020-05-11 13:26:56 -0500

    move avatars and their styles to deps

---
+++ avatar/demo/demo.js
@@ -0,0 +1,28 @@
+import CanMap from 'can-map'
+import Component from 'can-component'
+import template from './demo.stache'
+
+import 'can-map-define'
+
+let AvatarDemoVM = CanMap.extend({
+  define: {
+    gender: {
+      value: 'female'
+    },
+
+    facing: {
+      value: 'left'
+    },
+
+    skin: {
+      value: 'medium'
+    }
+  }
+})
+
+export default Component.extend({
+  view: template,
+  ViewModel: AvatarDemoVM,
+  tag: 'a2j-viewer-avatar-demo',
+  leakScope: false
+})
